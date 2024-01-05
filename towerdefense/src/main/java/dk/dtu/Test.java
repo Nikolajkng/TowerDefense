0bd72f14ec01db918dcfc250ff750eafea647ced
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2024-01-05 14:46:11 +0100

    Added a Test.java file to test javafx via maven

--- towerdefense/src/main/java/dk/dtu/Test.java
+++ towerdefense/src/main/java/dk/dtu/Test.java
@@ -12,7 +12,7 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Test extends Application {
 
     public static void main(String[] args) throws InterruptedException {
         Space inbox = new SequentialSpace();
