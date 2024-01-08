ff90b9bcfc49d5b3dcc37eb6b703dffcf576b8f6
michmai <121814049+michmai@users.noreply.github.com>
2024-01-08 18:35:03 +0100

    .

--- towerdefense/src/main/java/dk/dtu/app/view/Board.java
+++ towerdefense/src/main/java/dk/dtu/app/view/Board.java
@@ -8,13 +8,12 @@ public class Board {
     public static void createPlayerBoard(GridPane myBoard, int cellSize, int numOfCellsX, int numOfCellsY) {
         
         MyButton[][] board = new MyButton[1400][900];
-        int buttonSize = cellSize;
 
         for(int x = 0; x < numOfCellsX; x++){
             for(int y = 0; y < numOfCellsY; y++){
                 // Create a new button which represents each cell on the board
                 MyButton cell = new MyButton(0);
-                cell.setPrefSize(buttonSize, buttonSize);
+                cell.setPrefSize(cellSize, cellSize);
                 // Add the button to the grid and a corresponding coordinate position
                 myBoard.add(cell, x, y);
                 board[x][y] = cell;
