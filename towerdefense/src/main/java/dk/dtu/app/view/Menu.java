b401e4341c329dc96175848ae8d60bffa48e9722
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2024-01-08 23:13:01 +0100

    Update Menu.java

--- towerdefense/src/main/java/dk/dtu/app/view/Menu.java
+++ towerdefense/src/main/java/dk/dtu/app/view/Menu.java
@@ -10,7 +10,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Menu extends Application{
