40703fd3e70fca36002acdf052c3263a6d5069b2
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2024-01-08 11:43:31 +0100

    Added comments and moved code lines (no new code)

--- towerdefense/src/main/java/dk/dtu/Server.java
+++ towerdefense/src/main/java/dk/dtu/Server.java
@@ -11,23 +11,27 @@ import org.jspace.SpaceRepository;
 public class Server {
     private static String ip;
     public static void main(String[] args) throws UnknownHostException, SocketException {
+        
+        // IPv4 address retrieval code by Mr.Wang from Next Door 
+        // Link to source: https://stackoverflow.com/questions/9481865/getting-the-ip-address-of-the-current-machine-using-java
         try (final DatagramSocket socket = new DatagramSocket()) {
             socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
             ip = socket.getLocalAddress().getHostAddress();
             
         }
 
+        // Gate URI 
         int port = 55000;
         String gate_uri = "tcp://"+ ip + ":" + port + "/?keep";
-        
+        System.out.println(gate_uri);
         
         SpaceRepository server = new SpaceRepository();
+        server.addGate(gate_uri);
 
         String spaceName = "Room";
         server.add(spaceName, new SequentialSpace());
         String remote_uri_room = "tcp://"+ ip + ":" + port + "/" + spaceName + "?keep";
 
         
-        server.addGate(gate_uri);
     }
 }
