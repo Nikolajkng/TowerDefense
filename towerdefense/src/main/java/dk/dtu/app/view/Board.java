ad8de696dd08d602ef0e442bb2db2c93182d867d
michmai <121814049+michmai@users.noreply.github.com>
2024-01-08 18:31:53 +0100

    .

--- towerdefense/src/main/java/dk/dtu/app/view/Board.java
+++ towerdefense/src/main/java/dk/dtu/app/view/Board.java
@@ -5,17 +5,17 @@ import javafx.scene.layout.GridPane;
 
 public class Board {
     // Creating the player boards
-    public static void createPlayerBoard(GridPane myBoard) {
+    public static void createPlayerBoard(GridPane myBoard, int cellSize) {
         
         MyButton[][] board = new MyButton[1400][900];
         int numOfCells = 10;
-        int buttonSize = 40;
+        int buttonSize = cellSize;
 
-        for(int x = 0; x < numOfCells; x++){
-            for(int y = 0; y < numOfCells+4; y++){
+        for(int x = 0; x < numOfCells+4; x++){
+            for(int y = 0; y < numOfCells; y++){
                 // Create a new button which represents each cell on the board
                 MyButton cell = new MyButton(0);
-                cell.setPrefSize(20, 20);
+                cell.setPrefSize(buttonSize, buttonSize);
                 // Add the button to the grid and a corresponding coordinate position
                 myBoard.add(cell, x, y);
                 board[x][y] = cell;
--- towerdefense/src/main/java/dk/dtu/app/view/SingleplayerBoard.java
+++ towerdefense/src/main/java/dk/dtu/app/view/SingleplayerBoard.java
@@ -54,6 +54,6 @@ public class SingleplayerBoard extends Application{
 
         leftVbox.getChildren().addAll(btn3);
 
-        Board.createPlayerBoard(pane);
+        Board.createPlayerBoard(pane,86);
             }  
         }       
