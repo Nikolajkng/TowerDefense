38f910dfb326041775182ab5b060c381ebe0a3de
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2024-01-09 10:58:16 +0100

    added buttonOnAction

--- towerdefense/src/main/java/dk/dtu/app/view/Board.java
+++ towerdefense/src/main/java/dk/dtu/app/view/Board.java
@@ -1,15 +1,19 @@
 package dk.dtu.app.view;
 
 import dk.dtu.app.controller.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.layout.GridPane;
 
 public class Board {
     // Creating the player boards
     public static void createPlayerBoard(GridPane myBoard, int cellSize, int numOfCellsX, int numOfCellsY, int value) {
-        
+
         MyButton[][] board = new MyButton[1400][900];
-        for(int x = 0; x < numOfCellsX; x++){
-            for(int y = 0; y < numOfCellsY; y++){
+        int x = 0;
+        int y = 0;
+        for (x = 0; x < numOfCellsX; x++) {
+            for (y = 0; y < numOfCellsY; y++) {
                 // Create a new button which represents each cell on the board
                 MyButton cell = new MyButton(value);
                 cell.setPrefSize(cellSize, cellSize);
@@ -17,13 +21,18 @@ public class Board {
                 myBoard.add(cell, x, y);
                 board[x][y] = cell;
 
-                board[x][y].setOnAction(e -> {
-                    System.out.println("Button clicked");
-                    System.out.println("Value: " + board[x][y].getValue());
-                    System.out.println("X: " + x + " Y: " + y);
-                    System.out.println("Cell size: " + cellSize);
+                board[x][y].setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent e) {
+                        clickInfo(x, y);
+                    }
                 });
             }
         }
-            }
-        }
+    }
+
+    private static void clickInfo(int x, int y) {
+        System.out.println("Button clicked");
+        System.out.println("X: " + x + " Y: " + y);
+    }
+}
