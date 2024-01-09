dd6109a60afc5666e67810880e4f1637bcbecd82
michmai <121814049+michmai@users.noreply.github.com>
2024-01-09 12:16:47 +0100

    .

--- towerdefense/src/main/java/dk/dtu/app/view/Board.java
+++ towerdefense/src/main/java/dk/dtu/app/view/Board.java
@@ -13,6 +13,10 @@ public class Board {
         int x = 0;
         int y = 0;
 
+        int[] pathX = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 5, 6, 7, 7, 7, 7, 7, 7, 7, 7, 8, 9, 10, 10, 10, 10, 10, 11, 12, 13 };
+                
+        int[] pathY = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 8, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 8, 8, 8, 7, 6, 5, 4, 4, 4, 4, 4, 4 };
+
         // Create the gameboard
         for (x = 0; x < numOfCellsX; x++) {
             for (y = 0; y < numOfCellsY; y++) {
@@ -20,8 +24,23 @@ public class Board {
                 MyButton cell = new MyButton(value);
                 cell.setPrefSize(cellSize, cellSize);
 
-                // Set the path to be a different color
-                setPath(cell, x, y);
+                boolean isPath = false;
+                for (int i = 0; i < pathX.length; i++){
+                    if ( x == pathX[i] && y == pathY[i]){ //checks the coordinate of the path 
+                        isPath = true;
+                        break;
+                    }
+                }
+
+                if(isPath){
+                    cell.setStyle("-fx-background-color: #DEB887;" // Path color
+                    + "-fx-border-color: black;"
+                    + " -fx-border-width: 1;");
+                } else {
+                    cell.setStyle("-fx-background-color: linear-gradient(to bottom, green, #33CC66);"
+                    + "-fx-border-color: black;"
+                    + " -fx-border-width: 1;");
+                }
 
                 // Add the button to the grid and a corresponding coordinate position
                 myBoard.add(cell, x, y);
@@ -46,34 +65,4 @@ public class Board {
         System.out.println("X: " + x + " Y: " + y);
         System.out.println("Value: " + board[x][y].getValue());
     }
-
-    public static void setPath(MyButton cell, int x, int y) {
-
-        int[] pathX = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 5, 6, 7, 7, 7, 7, 7, 7, 7, 7, 8, 9, 10,
-                10, 10, 10, 10, 11, 12, 13 };
-                
-        int[] pathY = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 8, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 8, 8, 8,
-                7, 6, 5, 4, 4, 4, 4, 4, 4 };
-
-        boolean isPath = false;
-        for (int i = 0; i > pathX.length; i++) {
-            if (x == pathX[i] && y == pathY[i]) {
-                isPath = true;
-                break;
-            }
-        }
-        if (isPath) {
-            cell.setStyle(
-                    "-fx-background-color: #DEB887;"
-                            + "-fx-border-color: black;"
-                            + " -fx-border-width: 1;");
-        } else {
-            cell.setStyle(
-                    "-fx-background-color: linear-gradient(to bottom, green, #33CC66);"
-                            + "-fx-border-color: black;"
-                            + " -fx-border-width: 1;");
-        }
-
-    }
-
 }
