1a0e46385ab7cb3d08ea30540c71940cbf0c4e9d
michmai <121814049+michmai@users.noreply.github.com>
2024-01-09 10:51:17 +0100

    board maze path

--- towerdefense/src/main/java/dk/dtu/app/view/Board.java
+++ towerdefense/src/main/java/dk/dtu/app/view/Board.java
@@ -7,6 +7,10 @@ public class Board {
     public static void createPlayerBoard(GridPane myBoard, int cellSize, int numOfCellsX, int numOfCellsY) {
         
         MyButton[][] board = new MyButton[1400][900];
+
+        int[] pathX = {1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 5, 6, 7, 7, 7, 7, 7, 7, 7, 7, 8, 9, 10, 10, 10, 10, 10, 11, 12, 13};
+        int[] pathY = {0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 8, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 8, 8, 8, 7, 6, 5, 4, 4, 4, 4, 4, 4};
+
         for(int x = 0; x < numOfCellsX; x++){
             for(int y = 0; y < numOfCellsY; y++){
                 // Create a new button which represents each cell on the board
@@ -14,22 +18,40 @@ public class Board {
                 cell.setPrefSize(cellSize, cellSize);
 
                 // Background
-				if (numOfCellsX % 2 == 0 && numOfCellsY % 2 == 0) {
+				/*if (numOfCellsX % 2 == 0 && numOfCellsY % 2 == 0) {
 					cell.setStyle("-fx-background-color: linear-gradient(to bottom, green, #33CC66);"
                     + "-fx-border-color: black;"
                     + " -fx-border-width: 1;"
                    ); // Button color 1
 				} else {
-					cell.setStyle("-fx-base: #D2B48C;"); // Button color 2
+					cell.setStyle("-fx-base: #A52A2A;"); // Button color 2
 				}
 				if (numOfCellsX % 2 != 0 && numOfCellsY % 2 != 0) {
-					cell.setStyle("-fx-base: #8B4513"); // Button color 1
-				}
+					cell.setStyle("-fx-base: #A52A2A"); // Button color 1
+				}*/
+
+                boolean isPath = false;
+                for (int i = 0; i < pathX.length; i++){
+                    if ( x == pathX[i] && y == pathY[i]){
+                        isPath = true;
+                        break;
+                    }
+                }
+
+                if(isPath){
+                    cell.setStyle("-fx-background-color: #DEB887;" // Path color
+                    + "-fx-border-color: black;"
+                    + " -fx-border-width: 1;");
+                } else {
+                    cell.setStyle("-fx-background-color: linear-gradient(to bottom, green, #33CC66);"
+                    + "-fx-border-color: black;"
+                    + " -fx-border-width: 1;");
+                }
 
                 // Add the button to the grid and a corresponding coordinate position
                 myBoard.add(cell, x, y);
                 board[x][y] = cell;
             }
         }
-}
+    }
 }
