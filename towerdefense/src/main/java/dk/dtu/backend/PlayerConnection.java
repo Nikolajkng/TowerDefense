b1e1ea06c8f99ca22d88efd6fb7d6f0974a5873a
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2024-01-09 10:23:07 +0100

    Working on a connection between two player

--- towerdefense/src/main/java/dk/dtu/backend/PlayerConnection.java
+++ towerdefense/src/main/java/dk/dtu/backend/PlayerConnection.java
@@ -1,15 +1,59 @@
 package dk.dtu.backend;
 
+import java.io.IOException;
+
+import org.jspace.*;
+
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextInputDialog;
 
-public class PlayerConnection{
+public class PlayerConnection {
     public static String inputIP = "";
 
-    public static void joinGame(ActionEvent event) {
+    public static void hostGame(ActionEvent event) {
+        System.out.println("Hosting game...");
+        Server.hostNewGame();
+        System.out.println("efter hostnewGame()");
+        try {
+
+            Server.room.put("join", "player1");
+            System.out.println("Player 1 has joined the room");
+
+             // Waiting on player 2 to join room
+            Server.room.get(new ActualField ("join"), new ActualField("player2"));
+            System.out.println("Player 2 has now joined the room");
+
+            // Game start when true
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    
+    public static void joinGame(ActionEvent event) throws InterruptedException {
         System.out.println("Joining game...");
 
         // Code here:
+        showTextInputDialog();
+        String URIformat = "tcp://" + inputIP + ":55000/Room?keep";
+
+        // Connect to the room
+        try {
+            System.out.println("Connecting to room at: " + URIformat);
+            RemoteSpace myRoom = new RemoteSpace(URIformat);
+            myRoom.put("join", "player2");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+
+    private static void showTextInputDialog() {
         TextInputDialog dialog = new TextInputDialog();
         dialog.setTitle("Joining Game");
         dialog.setHeaderText(null); // Must be null, otherwise the header text will be displayed twice
@@ -18,18 +62,7 @@ public class PlayerConnection{
         // Show the dialog and wait for the user's response
         dialog.showAndWait().ifPresent(input -> {
             inputIP = input;
-            System.out.println("Entered IP is: " + inputIP);
-            // Process the userIP here:
-
         });
-
-    }
-
-    public static void hostGame(ActionEvent event) {
-        System.out.println("Hosting game...");
-
-        // Code here:
-        
 
     }
 }
--- towerdefense/src/main/java/dk/dtu/backend/Server.java
+++ towerdefense/src/main/java/dk/dtu/backend/Server.java
@@ -1,31 +1,34 @@
 package dk.dtu.backend;
 
-import java.net.SocketException;
-import java.net.UnknownHostException;
-
 import org.jspace.SequentialSpace;
 import org.jspace.SpaceRepository;
 
 public class Server {
     private static String ip;
-    public static void main(String[] args) throws UnknownHostException, SocketException {
+    public static SequentialSpace room = new SequentialSpace();
+
+
+    public static void hostNewGame(){
         // Local IPv4 address
         ip = LocalAddressScript.getLocalAddress(); 
 
-        // Gate URI 
+        // Gate URI into spaceRepository
         int port = 55000;
         String gate_uri = "tcp://"+ ip + ":" + port + "/?keep";
         System.out.println("Gate URI:"+gate_uri);
         
+        //Creating a new spaceRepository
         SpaceRepository server = new SpaceRepository();
         server.addGate(gate_uri);
 
+        // Creating a room inside the spacerepository
         String spaceName = "Room";
-        server.add(spaceName, new SequentialSpace());
+        server.add(spaceName, room);
+
+        // Creating a gateway into the room via new URI
         String room_uri = "tcp://"+ ip + ":" + port + "/" + spaceName + "?keep";
         System.out.println("Room URI"+room_uri);
 
-        // Creating a new thread to handle player joining
-                        
     }
+
 }
