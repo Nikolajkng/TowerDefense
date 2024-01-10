1038400975e80b8effade6b1ab9f4d555c6fa0a8
Fewrick <frederikudby556@gmail.com>
2024-01-10 16:59:54 +0100

    Update Chat.java

--- towerdefense/src/main/java/dk/dtu/backend/Chat.java
+++ towerdefense/src/main/java/dk/dtu/backend/Chat.java
@@ -1,6 +1,8 @@
 package dk.dtu.backend;
 
 import java.io.IOException;
+import java.net.UnknownHostException;
+import java.rmi.Remote;
 
 import org.jspace.ActualField;
 import org.jspace.FormalField;
@@ -13,39 +15,41 @@ import javafx.scene.control.Label;
 
 public class Chat implements Runnable {
     String callsign;
+    RemoteSpace chatRoom;
 
-    public Chat(String callsign) {
+    public Chat(String callsign) throws UnknownHostException, IOException {
         this.callsign = callsign;
+        this.chatRoom = new RemoteSpace("tcp://" + PlayerConnection.inputIP + ":55000/chatRoom?keep");
+
     }
 
     @Override
     public void run() {
-        while(true){
-        try {
-            RemoteSpace chatRoom = new RemoteSpace("tcp://" + PlayerConnection.inputIP + ":55000/chatRoom?keep");
-            System.out.println(PlayerConnection.inputIP);
-            if(callsign == "Host"){
-                while(true){
-                    Object[] message = chatRoom.get(new ActualField("client"), new FormalField(String.class));
-                    System.out.println("Client: " + (String) message[1]);
-                    Platform.runLater(() -> {
-                        ChatGUI.messageList.add(new Label("Player 2: "+ (String) message[1]));
-                    });
+        while (true) {
+            try {
+                System.out.println(PlayerConnection.inputIP);
+                if (callsign == "Host") {
+                    while (true) {
+                        Object[] message = chatRoom.get(new ActualField("client"), new FormalField(String.class));
+                        System.out.println("Client: " + (String) message[1]);
+                        Platform.runLater(() -> {
+                            ChatGUI.messageList.add(new Label("Player 2: " + (String) message[1]));
+                        });
+                    }
+                } else if (callsign == "client") {
+                    while (true) {
+                        Object[] message = chatRoom.get(new ActualField("Host"), new FormalField(String.class));
+                        System.out.println("Host: " + (String) message[1]);
+                        Platform.runLater(() -> {
+                            ChatGUI.messageList.add(new Label("Player 1: " + (String) message[1]));
+                        });
+                    }
                 }
-            } else if(callsign == "client"){
-                while(true){
-                    Object[] message = chatRoom.get(new ActualField("Host"), new FormalField(String.class));
-                    System.out.println("Host: " + (String) message[1]);
-                Platform.runLater(() -> {
-                        ChatGUI.messageList.add(new Label("Player 1: "+ (String) message[1]));
-                    });                }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-
-
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
+            System.out.println("Chat started");
         }
-        System.out.println("Chat started");
-    }
     }
 }
