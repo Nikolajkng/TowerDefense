fa2ff16b872ecd32828000646615802509a4c7f3
Stonehench <121813975+Stonehench@users.noreply.github.com>
2024-01-10 12:28:47 +0100

    fejl

--- towerdefense/src/main/java/dk/dtu/app/view/GameBoardsGUI/SingleplayerBoard.java
+++ towerdefense/src/main/java/dk/dtu/app/view/GameBoardsGUI/SingleplayerBoard.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
