ff344de72e6eb1d2694a1b3ba64272d208ddde26
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2024-01-10 19:54:15 +0100

    Fix - SingleplayerBoard construction error

--- towerdefense/src/main/java/dk/dtu/app/view/GameBoardsGUI/SingleplayerBoard.java
+++ towerdefense/src/main/java/dk/dtu/app/view/GameBoardsGUI/SingleplayerBoard.java
@@ -126,7 +126,7 @@ public class SingleplayerBoard extends Application {
         //add enemie to the gridpane at cell (0,0)
         pane.add(gifView, 0, 1);
         
-        Board.createPlayerBoard(pane, 86, 14, 10, 0);
+        BoardController.createPlayerBoard(pane, 86, 14, 10, 0);
     }
 
    // Create a circle to be used as the button's shape
