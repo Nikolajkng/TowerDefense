0790c5ed591b0c45034de67b05fe409d6c4361ab
michmai <121814049+michmai@users.noreply.github.com>
2024-01-10 12:49:15 +0100

    .

--- towerdefense/src/main/java/dk/dtu/app/view/GameBoardsGUI/SingleplayerBoard.java
+++ towerdefense/src/main/java/dk/dtu/app/view/GameBoardsGUI/SingleplayerBoard.java
@@ -10,13 +10,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Polygon;
-import javafx.scene.shape.SVGPath;
-import javafx.scene.shape.Shape;
 
 public class SingleplayerBoard extends Application {
 
