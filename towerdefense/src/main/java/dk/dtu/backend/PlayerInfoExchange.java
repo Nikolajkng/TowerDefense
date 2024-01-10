174232d584fcd16e1b3f4b92842133c92bddfad0
Fewrick <frederikudby556@gmail.com>
2024-01-10 10:56:02 +0100

    testing players receiving actions

--- towerdefense/src/main/java/dk/dtu/backend/PlayerInfoExchange.java
+++ towerdefense/src/main/java/dk/dtu/backend/PlayerInfoExchange.java
@@ -3,6 +3,8 @@ package dk.dtu.backend;
 import java.io.IOException;
 import java.net.UnknownHostException;
 
+import org.jspace.ActualField;
+import org.jspace.FormalField;
 import org.jspace.RemoteSpace;
 
 public class PlayerInfoExchange {
@@ -14,17 +16,31 @@ public class PlayerInfoExchange {
         playerInfoExchange = new RemoteSpace(roomURI);
     }
 
-
     public static void sendAction(int x, int y, String action) {
         try {
-            System.out.println("X: "+ x +" Y: "+ y +" - Selected following: "+ action);
-            playerInfoExchange.put(x,y, action);
-            
+            System.out.println("X: " + x + " Y: " + y + " - Selected following: " + action);
+            playerInfoExchange.put(x, y, action);
 
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
     }
-    
+
+    public static void receiveActions() {
+        try {
+            Object[] info = playerInfoExchange.queryp(new FormalField(Integer.class), new FormalField(Integer.class),
+                    new FormalField(String.class));
+            if (info != null) {
+                while (true) {
+                    Object[] action = playerInfoExchange.get(new FormalField(Integer.class), new FormalField(Integer.class),
+                    new FormalField(String.class));
+                System.out.println("Received action: " +  (String) action[2]);
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
