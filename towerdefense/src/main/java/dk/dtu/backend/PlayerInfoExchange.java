e01389320d79de0c57338a4e2df4aeb4bf799b6d
Fewrick <frederikudby556@gmail.com>
2024-01-10 11:05:17 +0100

    Update PlayerInfoExchange.java

--- towerdefense/src/main/java/dk/dtu/backend/PlayerInfoExchange.java
+++ towerdefense/src/main/java/dk/dtu/backend/PlayerInfoExchange.java
@@ -30,11 +30,21 @@ public class PlayerInfoExchange {
     public static void receiveActions() {
         try {
             while (true) {
-                Object[] action = playerInfoExchange.get(new FormalField(Integer.class), new FormalField(Integer.class),
+                Object[] info = playerInfoExchange.queryp(new FormalField(Integer.class),
+                        new FormalField(Integer.class),
                         new FormalField(String.class));
-                System.out.println("Received action: " + (String) action[2]);
+                if (info != null) {
+                    Object[] action = playerInfoExchange.get(new FormalField(Integer.class),
+                            new FormalField(Integer.class),
+                            new FormalField(String.class));
+                    System.out.println("Received action: " + (String) action[2]);
+
+                }
             }
-        } catch (InterruptedException e) {
+        } catch (
+
+        InterruptedException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
