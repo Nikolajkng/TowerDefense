f4bfa65333c567194d1bd5803c805248334e05a9
Fewrick <frederikudby556@gmail.com>
2024-01-10 15:12:52 +0100

    Update MultiplayerBoard.java

--- towerdefense/src/main/java/dk/dtu/app/view/GameBoardsGUI/MultiplayerBoard.java
+++ towerdefense/src/main/java/dk/dtu/app/view/GameBoardsGUI/MultiplayerBoard.java
@@ -14,7 +14,6 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class MultiplayerBoard extends Application {
@@ -26,6 +25,13 @@ public class MultiplayerBoard extends Application {
     public static Button towerBtn1 = new Button("Tower1 $0");
     public static Button towerBtn2 = new Button("Tower2 $0");
     public static Button towerBtn3 = new Button("Tower3 $0");
+    public static Button towerBtn4 = new Button("Tower4 $0");
+    public static Button towerBtn5 = new Button("Tower5 $0");
+    public static Button attackEnemy1btn = new Button("Attack enemy");
+    public static Button attackEnemy2btn = new Button("Attack enemy");
+    public static Button attackEnemy3btn = new Button("Attack enemy");
+    public static Button attackEnemy4btn = new Button("Attack enemy");
+    public static Button attackEnemy5btn = new Button("Attack enemy");
     public static Label healthP1 = new Label("100");
     public static Label healthP2 = new Label("100");
     public static Label topTitle = new Label("RABBIT HUNTER");
@@ -37,11 +43,9 @@ public class MultiplayerBoard extends Application {
     int sizeY = 1000; // MyButton cell = new MyButton(0);
     Button btn3 = new Button("333");
     Button btn4 = new Button("4444");
-    public static Button towerBtn4 = new Button("Tower4 $0");
-    public static Button towerBtn5 = new Button("Tower5 $0");
+
     private VBox chatBox = new VBox(5);
     private List<Label> messages = new ArrayList<>();
-    
 
     // Program start
     @Override
@@ -71,7 +75,7 @@ public class MultiplayerBoard extends Application {
         // Positions of all panes
         centerPane.setAlignment(Pos.CENTER);
         centerPane.setSpacing(100);
-        
+
         borderPane.setLeft(leftVbox);
         borderPane.setRight(rightVbox);
         borderPane.setCenter(centerPane);
@@ -80,6 +84,8 @@ public class MultiplayerBoard extends Application {
         bottomHUD.setAlignment(Pos.BOTTOM_CENTER);
         leftVbox.setAlignment(Pos.TOP_CENTER);
         leftVbox.setSpacing(30);
+        rightVbox.setAlignment(Pos.TOP_CENTER);
+        rightVbox.setSpacing(30);
 
         // Size of boxPanes
         leftVbox.setPrefWidth(sizeX / 8 - 25);
@@ -87,20 +93,20 @@ public class MultiplayerBoard extends Application {
         bottomHUD.setPrefHeight(sizeY / 8 + 25);
         topBar.setPrefHeight(sizeY / 8 + 25);
 
-                
         // Left vbox-menu setup:
         leftVbox.getChildren().addAll(towerBtn1, towerBtn2, towerBtn3, towerBtn4, towerBtn5);
+
+        // Right vbox-menu setup
+        rightVbox.getChildren().addAll(attackEnemy1btn,attackEnemy2btn,attackEnemy3btn,attackEnemy4btn,attackEnemy5btn);
 
         // Chatbox:
         Label msg1 = new Label("hej");
         Label msg2 = new Label("farvel");
-        
+
         bottomHUD.getChildren().addAll(chatBox);
         chatBox.getChildren().addAll(msg1, msg2);
         chatBox.setPrefSize(400, 100);
         chatBox.setStyle("-fx-background-color: #D7E0E0");
-
-
 
         // Button sizes
         int towerBtnWidth = 120;
@@ -110,7 +116,11 @@ public class MultiplayerBoard extends Application {
         towerBtn3.setPrefSize(towerBtnWidth, towerBtnHeight);
         towerBtn4.setPrefSize(towerBtnWidth, towerBtnHeight);
         towerBtn5.setPrefSize(towerBtnWidth, towerBtnHeight);
-
+        attackEnemy1btn.setPrefSize(towerBtnWidth, towerBtnHeight);
+        attackEnemy2btn.setPrefSize(towerBtnWidth, towerBtnHeight);
+        attackEnemy3btn.setPrefSize(towerBtnWidth, towerBtnHeight);
+        attackEnemy4btn.setPrefSize(towerBtnWidth, towerBtnHeight);
+        attackEnemy5btn.setPrefSize(towerBtnWidth, towerBtnHeight);
 
         // Scene setup
         Scene scene = new Scene(borderPane, sizeX, sizeY);
@@ -128,7 +138,7 @@ public class MultiplayerBoard extends Application {
         healthP1.setStyle("-fx-background-color: #ffffff; -fx-font-size: 40; -fx-text-fill: #ff0000");
         healthP2.setStyle("-fx-background-color: #ffffff; -fx-font-size: 40; -fx-text-fill: #ff0000");
         topTitle.setStyle("-fx-font-size: 60; -fx-text-fill: #ff9966");
-        
+
         // Creating boards for two players
         leftBoard = Board.createPlayerBoard(leftPane, 100, 14, 10, 0);
         rightBoard = Board.createPlayerBoard(rightPane, 100, 14, 10, -1);
