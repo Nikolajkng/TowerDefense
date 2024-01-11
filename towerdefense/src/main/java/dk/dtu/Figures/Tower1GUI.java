5128b8869db3d21e8d813cb58c32021a0f31dd0f
michmai <121814049+michmai@users.noreply.github.com>
2024-01-11 12:56:13 +0100

    .

--- towerdefense/src/main/java/dk/dtu/Figures/Tower1GUI.java
+++ towerdefense/src/main/java/dk/dtu/Figures/Tower1GUI.java
@@ -1,5 +1,11 @@
 package dk.dtu.Figures;
 
+import javafx.scene.control.Button;
+
 public class Tower1GUI {
+    Button Tower1 = createRoundButton("/dk/dtu/app/view/Images/ZdPH.gif");
+    Button Tower2 = createRoundButton("/dk/dtu/app/view/Images/giphy.gif");
+    Button Tower3 = createRoundButton("/dk/dtu/app/view/Images/SYKT7E.gif");
+
     
 }
