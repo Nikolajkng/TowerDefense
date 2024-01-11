bacdbbff61b0b5322564745bf54ff59f3bfc0553
michmai <121814049+michmai@users.noreply.github.com>
2024-01-11 11:14:50 +0100

    .

--- towerdefense/src/main/java/dk/dtu/backend/PlayerInfo.java
+++ towerdefense/src/main/java/dk/dtu/backend/PlayerInfo.java
@@ -2,4 +2,14 @@ package dk.dtu.backend;
 
 public class PlayerInfo {
     
+    int Life = 10;
+    int Money = 100;
+
+    public static void lifeTracker(){
+
+    }
+
+    public static void moneyTracker(){
+
+    }
 }
