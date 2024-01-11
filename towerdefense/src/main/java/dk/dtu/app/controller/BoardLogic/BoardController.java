c56ad92b02a07f5a652d8b9e0691d5398d5edb63
michmai <121814049+michmai@users.noreply.github.com>
2024-01-11 17:29:53 +0100

    place tower is now available

--- towerdefense/src/main/java/dk/dtu/app/controller/BoardLogic/BoardController.java
+++ towerdefense/src/main/java/dk/dtu/app/controller/BoardLogic/BoardController.java
@@ -5,6 +5,8 @@ import javax.swing.text.View;
 import dk.dtu.app.controller.*;
 import dk.dtu.app.controller.Tower.ActionType;
 import dk.dtu.app.view.Figures.Enemy1_BunnyGUI;
+import dk.dtu.app.view.Figures.PlaceTower;
+import dk.dtu.app.view.Figures.Tower1GUI;
 import dk.dtu.backend.PlayerConnection;
 import dk.dtu.backend.ActionSender;
 import javafx.event.ActionEvent;
@@ -72,10 +74,12 @@ public class BoardController {
                         if(board[finalX][finalY].getValue() == -2){
                             Enemy1_BunnyGUI.placeBunny(cell);
                         }
-                        if(board[finalX][finalY].getValue() != -1){
+                        if(board[finalX][finalY].getValue() != -2){
+                            
                             clickInfo(board, finalX, finalY);
                             // "if wish to place tower, then execute Tower.placeTower action"
                             if(true){
+                                Tower1GUI.placePlant1(cell);
                                 Tower.placeTower(finalX, finalY, board, type);
                                 ActionSender.sendAction(finalX, finalY, type, callsign);
                             }
