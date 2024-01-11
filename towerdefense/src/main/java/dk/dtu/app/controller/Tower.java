97868f7c1d8ac3df0337e768ff3de14f4ddfcb6d
Fewrick <frederikudby556@gmail.com>
2024-01-11 10:47:38 +0100

    Update Tower.java

--- towerdefense/src/main/java/dk/dtu/app/controller/Tower.java
+++ towerdefense/src/main/java/dk/dtu/app/controller/Tower.java
@@ -1,27 +1,22 @@
 package dk.dtu.app.controller;
 
-
 public class Tower {
     public static void placeTower(int x, int y, MyButton[][] board, String action) {
 
-        System.out.println("Placing tower at: (" + x + ", " + y+") with action: " + action);
-        // Check if tower can be placed at x, y
-        if(true){
-            if(action == "tower1"){
-                board[x][y].setText("X");
-                board[x][y].setValue(-1);
-            };
-            if(action == "tower2"){
-                board[x][y].setText("O");
-                board[x][y].setValue(-1); 
-            };
-            if(action == "tower3"){
-                board[x][y].setText("±");
-                board[x][y].setValue(-1); 
-            };
+        System.out.println("Placing tower at: (" + x + ", " + y + ") with action: " + action);
 
+        if (action == "tower1") {
+            board[x][y].setText("X");
+            board[x][y].setValue(-1);
+        } else if (action == "tower2") {
+            board[x][y].setText("O");
+            board[x][y].setValue(-1);
+        } else if (action == "tower3") {
+            board[x][y].setText("±");
+            board[x][y].setValue(-1);
         } else {
-            System.out.println("Tower cannot be placed here");
+            System.out.println("Action does not match anything");
         }
-    }   
+
+    }
 }
