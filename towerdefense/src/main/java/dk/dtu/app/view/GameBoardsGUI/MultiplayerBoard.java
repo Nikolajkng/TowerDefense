8a85b43711476cf05b8c1c200a8c6ac6f073fd35
Fewrick <frederikudby556@gmail.com>
2024-01-12 16:54:44 +0100

    Update MultiplayerBoard.java

--- towerdefense/src/main/java/dk/dtu/app/view/GameBoardsGUI/MultiplayerBoard.java
+++ towerdefense/src/main/java/dk/dtu/app/view/GameBoardsGUI/MultiplayerBoard.java
@@ -99,7 +99,6 @@ public class MultiplayerBoard extends Application {
                 PlayerConnection.clientActionListenerThread.interrupt();
             }
 
-            System.exit(0);
 
         });
 
