54037d44002003b643d7da402890d890e10e0c44
Fewrick <frederikudby556@gmail.com>
2024-01-12 09:34:27 +0100

    Update Tower.java

--- towerdefense/src/main/java/dk/dtu/app/controller/Tower.java
+++ towerdefense/src/main/java/dk/dtu/app/controller/Tower.java
@@ -5,24 +5,20 @@ import dk.dtu.app.view.Figures.Tower1GUI;
 
 public class Tower {
 
-
-
     public static void placeTower(int x, int y, MyButton[][] board, Action.ActionType type) {
 
         System.out.println("Placing tower at: (" + x + ", " + y + ") with action: " + type);
 
         if (type == ActionType.TOWER1) {
-            Tower1GUI.placePlant1(tile);
+            Tower1GUI.placePlant1(board[x][y]);
             board[x][y].setText("X");
             board[x][y].setValue(-1);
-        }
-        else
-        if (type == ActionType.TOWER2) {
+        } else if (type == ActionType.TOWER2) {
+            Tower1GUI.placePlant1(board[x][y]);
             board[x][y].setText("O");
             board[x][y].setValue(-1);
-        }
-        else
-        if (type == ActionType.TOWER3) {
+        } else if (type == ActionType.TOWER3) {
+            Tower1GUI.placePlant1(board[x][y]);
             board[x][y].setText("±");
             board[x][y].setValue(-1);
         } else {
