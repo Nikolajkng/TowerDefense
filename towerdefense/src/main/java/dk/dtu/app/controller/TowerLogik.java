e71196a20d685dc7c2ea60ad714d393369b0058a
Stonehench <121813975+Stonehench@users.noreply.github.com>
2024-01-15 19:12:06 +0100

    kæmpe sejr, tårne kan se balloner og prøver at skyde dem hvis inden for rækkevide

--- towerdefense/src/main/java/dk/dtu/app/controller/TowerLogik.java
+++ towerdefense/src/main/java/dk/dtu/app/controller/TowerLogik.java
@@ -5,6 +5,7 @@ import org.jspace.FormalField;
 import org.jspace.Space;
 
 import java.awt.geom.Point2D;
+import java.util.List;
 
 abstract public class TowerLogik {
     private int coordinateX;
@@ -23,15 +24,18 @@ abstract public class TowerLogik {
 
     public void shoot(int radius, int damage) {
         try {
-            Object[] obj = space.queryp(new FormalField(Integer.class), new ActualField("Coordinates"),
+            List<Object[]> obj = space.queryAll(new FormalField(Integer.class), new ActualField("Coordinates"),
                     new FormalField(Double.class), new FormalField(Double.class));
             if (obj != null) {
-                System.out.println("saw some coordinates from " + (Integer) obj[0]);
-                double distance = Point2D.distance(coordinateX, coordinateY, (Double) obj[2], (Double) obj[3]);
-                System.out.println("Distance to target " + distance);
-                if (distance <= radius) {
-                    System.out.println("tower shoots at " + (Integer) obj[0]);
-                    space.put((Integer) obj[0], "Damage", damage);
+                for (int i = 0; i < obj.size(); i++) {
+                    System.out.println("saw some coordinates from " + (Integer) obj.get(i)[0]);
+                    double distance = Point2D.distance(coordinateX, coordinateY, (Double) obj.get(i)[2], (Double) obj.get(i)[3]);
+                    System.out.println("Distance to target " + distance);
+                    if (distance <= radius) {
+                        System.out.println("tower shoots at " + (Integer) obj.get(1)[0]);
+                        space.put((Integer) obj.get(1)[0], "Damage", damage);
+                        break;
+                    }
                 }
             }
         } catch (InterruptedException e) {
