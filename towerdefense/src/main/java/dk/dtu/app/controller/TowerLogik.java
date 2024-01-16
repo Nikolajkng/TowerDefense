00f3771c6a3ceda7645feaa999bb41ecdfeccaad
Stonehench <121813975+Stonehench@users.noreply.github.com>
2024-01-16 21:56:27 +0100

    virker

--- towerdefense/src/main/java/dk/dtu/app/controller/TowerLogik.java
+++ towerdefense/src/main/java/dk/dtu/app/controller/TowerLogik.java
@@ -33,14 +33,18 @@ abstract public class TowerLogik {
                     new FormalField(Double.class), new FormalField(Double.class));
             if (obj != null) {
                 for (int i = 0; i < obj.size(); i++) {
-                    //System.out.println("saw some coordinates from " + (Integer) obj.get(i)[0]);
-                    double distance = Point2D.distance(coordinateX, coordinateY, (Double) obj.get(i)[2], (Double) obj.get(i)[3]);
-                    // System.out.println("Distance to target " + distance);
-                    if (distance <= radius) {
-                        //System.out.println("tower shoots at " + (Integer) obj.get(i)[0]);
-                        // space.put((Integer) obj.get(1)[0], "Damage", damage);
-                        MultiplayerBoard.projectile(coordinateX, coordinateY, (Double) obj.get(i)[2], (Double) obj.get(i)[3], board);
-                        break;
+                    if ((Integer) obj.get(i)[0] % 2 == 0) {
+                        // System.out.println("saw some coordinates from " + (Integer) obj.get(i)[0]);
+                        double distance = Point2D.distance(coordinateX, coordinateY, (Double) obj.get(i)[2],
+                                (Double) obj.get(i)[3]);
+                        // System.out.println("Distance to target " + distance);
+                        if (distance <= radius) {
+                            // System.out.println("tower shoots at " + (Integer) obj.get(i)[0]);
+                            // space.put((Integer) obj.get(1)[0], "Damage", damage);
+                            MultiplayerBoard.projectile(coordinateX, coordinateY, (Double) obj.get(i)[2],
+                                    (Double) obj.get(i)[3], board);
+                            break;
+                        }
                     }
                 }
             }
