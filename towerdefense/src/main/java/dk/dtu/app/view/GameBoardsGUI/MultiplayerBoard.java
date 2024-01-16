fd6d4e1daf5e72879d6b59185766df0b0ccd6e3e
michmai <121814049+michmai@users.noreply.github.com>
2024-01-16 17:33:14 +0100

    coin size

--- towerdefense/src/main/java/dk/dtu/app/view/GameBoardsGUI/MultiplayerBoard.java
+++ towerdefense/src/main/java/dk/dtu/app/view/GameBoardsGUI/MultiplayerBoard.java
@@ -102,11 +102,11 @@ public class MultiplayerBoard extends Application {
 
                 Image coin = new Image("/dk/dtu/app/view/Images/coin.png");
                 ImageView imageView = new ImageView(coin);
-                imageView.setFitWidth(40);
-                imageView.setFitHeight(40);
+                imageView.setFitWidth(20);
+                imageView.setFitHeight(20);
                 Button coinButton = new Button("" + PlayerInfo.getMoney(), imageView);
                 coinButton.setStyle("-fx-background-size: cover; -fx-background-color: transparent; "
-                                + "-fx-fill: white; -fx-font-size: 30px; -fx-font-family: 'Commic Sans MS'; -fx-font-weight: bold;");
+                                + "-fx-fill: white; -fx-font-size: 20px; -fx-font-family: 'Commic Sans MS'; -fx-font-weight: bold;");
                 coinButton.setPrefSize(140, 140);
 
                 // Left vbox-menu setup:
@@ -116,8 +116,11 @@ public class MultiplayerBoard extends Application {
                 VBox.setMargin(towerBtn3, new javafx.geometry.Insets(8, 0, 0, 0));
         
                 // Right vbox-menu setup
-                rightVbox.getChildren().addAll(coinButton);
-                VBox.setMargin(coinButton, new javafx.geometry.Insets(10, 5, 0, 0));
+                //rightVbox.getChildren().addAll(coinButton);
+                //VBox.setMargin(coinButton, new javafx.geometry.Insets(10, 5, 0, 0));
+
+                bottomHUD.getChildren().addAll(coinButton);
+                HBox.setMargin(towerBtn3, new javafx.geometry.Insets(800, 0, 5, 0));
 
                 // Button sizes
                 int towerBtnWidth = 115;
