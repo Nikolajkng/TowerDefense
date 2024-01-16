c81121c9f16dbef0effaf57d71834be0fae5789a
michmai <121814049+michmai@users.noreply.github.com>
2024-01-16 17:30:07 +0100

    .

--- towerdefense/src/main/java/dk/dtu/app/view/GameBoardsGUI/MultiplayerBoard.java
+++ towerdefense/src/main/java/dk/dtu/app/view/GameBoardsGUI/MultiplayerBoard.java
@@ -101,11 +101,11 @@ public class MultiplayerBoard extends Application {
 
                 Image coin = new Image("/dk/dtu/app/view/Images/coin.png");
                 ImageView imageView = new ImageView(coin);
-                imageView.setFitWidth(20);
-                imageView.setFitHeight(20);
+                imageView.setFitWidth(30);
+                imageView.setFitHeight(30);
                 Button coinButton = new Button("" + PlayerInfo.getMoney(), imageView);
                 coinButton.setStyle("-fx-background-size: cover; -fx-background-color: transparent; "
-                                + "-fx-fill: white; -fx-font-size: 20px; -fx-font-family: 'Commic Sans MS'; -fx-font-weight: bold;");
+                                + "-fx-fill: white; -fx-font-size: 30px; -fx-font-family: 'Commic Sans MS'; -fx-font-weight: bold;");
                 coinButton.setPrefSize(140, 140);
 
                 // Left vbox-menu setup:
@@ -114,12 +114,12 @@ public class MultiplayerBoard extends Application {
                 VBox.setMargin(towerBtn2, new javafx.geometry.Insets(8, 0, 0, 0));
                 VBox.setMargin(towerBtn3, new javafx.geometry.Insets(8, 0, 0, 0));
         
-                // Right vbox-menu setup
-                //rightVbox.getChildren().addAll(coinButton);
-               // VBox.setMargin(coinButton, new javafx.geometry.Insets(10, 5, 0, 0));
+                //Right vbox-menu setup
+                rightVbox.getChildren().addAll(coinButton);
+                VBox.setMargin(coinButton, new javafx.geometry.Insets(10, 5, 0, 0));
 
-                bottomHUD.getChildren().addAll(coinButton);
-                HBox.setMargin(coinButton, new javafx.geometry.Insets(0, 800, 30, 0));
+                //bottomHUD.getChildren().addAll(coinButton);
+                //HBox.setMargin(coinButton, new javafx.geometry.Insets(0, 800, 30, 0));
 
                 // Button sizes
                 int towerBtnWidth = 115;
@@ -156,9 +156,9 @@ public class MultiplayerBoard extends Application {
 
                 // Health styling
                 healthP1.setStyle(
-                                "-fx-fill: white; -fx-font-size: 20px; -fx-font-family: 'Commic Sans MS'; -fx-font-weight: bold;");
+                                "-fx-fill: white; -fx-font-size: 30px; -fx-font-family: 'Commic Sans MS'; -fx-font-weight: bold;");
                 healthP2.setStyle(
-                                "-fx-fill: white; -fx-font-size: 20px; -fx-font-family: 'Commic Sans MS'; -fx-font-weight: bold;");
+                                "-fx-fill: white; -fx-font-size: 30px; -fx-font-family: 'Commic Sans MS'; -fx-font-weight: bold;");
                 topTitle.setStyle(
                                 "-fx-fill: white; -fx-font-size: 60px; -fx-font-family: 'Commic Sans MS'; -fx-font-weight: bold;");
 
