1eb6d81e731d97125b0ffcbad6be944f5f55b643
Stonehench <121813975+Stonehench@users.noreply.github.com>
2024-01-17 10:33:10 +0100

    muligvis

--- towerdefense/src/main/java/dk/dtu/app/controller/Collision.java
+++ towerdefense/src/main/java/dk/dtu/app/controller/Collision.java
@@ -6,13 +6,14 @@ import dk.dtu.app.controller.BoardLogic.MyPane;
 import dk.dtu.app.controller.Enemy.Enemy;
 import dk.dtu.app.view.GameBoardsGUI.MultiplayerBoard;
 import dk.dtu.backend.PlayerInfo;
+import javafx.animation.PathTransition;
 import javafx.application.Platform;
 import javafx.scene.shape.Path;
 
 public class Collision {
 
     // Invoked under Projectile "boundsInParentProperty"
-    public static void checkForCollision(Path carrotShape, MyPane board) {
+    public static void checkForCollision(Path carrotShape, MyPane board, PathTransition pathT) {
         Iterator<Enemy> iterator;
         if (board == MultiplayerBoard.leftBoard) {
             iterator = MultiplayerBoard.leftEnemyList.iterator();
@@ -36,10 +37,16 @@ public class Collision {
                 // Update the correct board
                 Platform.runLater(() -> {
                     board.getChildren().remove(e.getEnemyShape());
-
                 });
 
                 MultiplayerBoard.changeMoney(10);
+
+                pathT.stop();
+                Platform.runLater(() -> {
+                    board.getChildren().remove(carrotShape);
+                });
+
+                break;
 
             } else {
                 //System.out.println("No collision detected");
